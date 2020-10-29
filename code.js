bc2b25093fd8d6b9b071277b63149b0a82a1fa98
lvoelz_wctc <luke.voelz@gmail.com>
2020-10-28 21:17:30 -0700

    Update for Lab 4

--- code.js
+++ code.js
@@ -1,22 +1,28 @@
-function calculateLateFees() {
+$(document).ready(function () {
+//event handlers
+    $("#button_fees").click(calculateLateFees);
+    $("#button_pizza").click(calculatePizzaCost);
 
-    var lateBooks = prompt("How many late days for books?");
-    var lateDvds = prompt("How many late days for DVDs?");
+    function calculateLateFees() {
 
-    var lateBooksTotal = lateBooks * .25;
-    var lateDvdsTotal = lateDvds * .5;
-    var lateTotal = lateBooksTotal + lateDvdsTotal;
+        var lateBooks = prompt("How many late days for books?");
+        var lateDvds = prompt("How many late days for DVDs?");
 
-    alert(`Total late fees are $${lateTotal}.`);
-}
+        var lateBooksTotal = lateBooks * .25;
+        var lateDvdsTotal = lateDvds * .5;
+        var lateTotal = lateBooksTotal + lateDvdsTotal;
 
-function calculatePizzaCost() {
+        alert(`Total late fees are $${lateTotal}.`);
+    }
 
-    var numToppings = prompt("How many toppings?");
-    var numPeople = prompt("How many people?");
-    var total = 15 + (numToppings * 1.25);
-    var costPerPerson = total / numPeople;
-    var costDisplay = costPerPerson.toFixed(2);
+    function calculatePizzaCost() {
 
-    alert(`Total per person is $${costDisplay}.`);
-}
+        var numToppings = prompt("How many toppings?");
+        var numPeople = prompt("How many people?");
+        var total = 15 + (numToppings * 1.25);
+        var costPerPerson = total / numPeople;
+        var costDisplay = costPerPerson.toFixed(2);
+
+        alert(`Total per person is $${costDisplay}.`);
+    }
+});
